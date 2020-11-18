b52d31effcd4930c811c1a8fb2dbaaac3f5c0723
Ramkishor H <ramhosamane@gmail.com>
2020-11-18 14:12:13 +0530

    corrected ritviks mistakes

--- IntReview/Home/views.py
+++ IntReview/Home/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    render(request, 'home.html')
+    return render(request, 'home.html')
     
 def signin(request):
-    render(request, 'signin.html')
+    return render(request, 'signin.html')
     
 def signup(request):
-    render(request,'signup.html')
+    return render(request,'signup.html')
